8aa74661298807e9421860f21119f288377847f5
CodeCoderCoding <1845371176@qq.com>
2022-01-04 22:01:22 +0800

    add Spring-Boot-Static

---
+++ A06-Spring-Boot-Static/src/test/java/com/supremepole/a06springbootstatic/A06SpringBootStaticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supremepole.a06springbootstatic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A06SpringBootStaticApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
