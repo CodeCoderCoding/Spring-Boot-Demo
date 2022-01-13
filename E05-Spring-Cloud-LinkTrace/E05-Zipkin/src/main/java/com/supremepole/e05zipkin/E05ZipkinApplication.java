823fdaf2b4ea442cbff605e016cf88b33c4d504a
CodeCoderCoding <1845371176@qq.com>
2022-01-13 22:07:12 +0800

    add Zipkin in Spring-Cloud-LinkTrace

---
+++ E05-Spring-Cloud-LinkTrace/E05-Zipkin/src/main/java/com/supremepole/e05zipkin/E05ZipkinApplication.java
@@ -0,0 +1,13 @@
+package com.supremepole.e05zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E05ZipkinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(E05ZipkinApplication.class, args);
+    }
+
+}
---
+++ E05-Spring-Cloud-LinkTrace/E05-Zipkin/src/test/java/com/supremepole/e05zipkin/E05ZipkinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supremepole.e05zipkin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class E05ZipkinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
