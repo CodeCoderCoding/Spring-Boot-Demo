59b6edad18ee2f92d92bb732650dca2dc38a1a73
CodeCoderCoding <1845371176@qq.com>
2022-09-07 14:37:38 +0800

    A12-Spring-Boot-Swagger2, update

---
+++ A12-Spring-Boot-Development/A12-Spring-Boot-Swagger2/src/main/java/com/supremepole/a12springbootswagger2/A12SpringBootSwagger2Application.java
@@ -0,0 +1,13 @@
+package com.supremepole.a12springbootswagger2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A12SpringBootSwagger2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(A12SpringBootSwagger2Application.class, args);
+    }
+
+}
