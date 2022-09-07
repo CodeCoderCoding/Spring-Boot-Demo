ee8f9befaf1cc7c5b8ee5f67df3f7f73dff89718
CodeCoderCoding <1845371176@qq.com>
2022-09-07 14:44:16 +0800

    F02-Spring-ConfigBean-JavaConfig, update

--- F02-Spring-IoC/F02-Spring-ConfigBean-JavaConfig/src/main/java/com/supremepole/javaconfig/JavaConfig.java
+++ F02-Spring-IoC/F02-Spring-ConfigBean-JavaConfig/src/main/java/com/supremepole/javaconfig/JavaConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Configuration;
 /**
  * @author CodeCoderCoding
  */
+//使用@Configuration注解表明当前类是一个配置类，这意味着这个类里可能有0个或者多个@Bean注解
+//此处没有使用包扫描，是因为所有的Bean都在此类中定义了
 @Configuration
 public class JavaConfig {
     @Bean//2
