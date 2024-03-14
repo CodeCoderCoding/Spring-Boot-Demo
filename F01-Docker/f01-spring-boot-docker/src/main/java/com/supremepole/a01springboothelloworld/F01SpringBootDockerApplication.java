350b02f55a124ad2b11bf8230bd61f91f34e170a
Neil <1845371176@qq.com>
2024-03-14 17:11:06 +0800

    add spring-boot-docker

---
+++ F01-Docker/f01-spring-boot-docker/src/main/java/com/supremepole/a01springboothelloworld/F01SpringBootDockerApplication.java
@@ -0,0 +1,13 @@
+package com.supremepole.a01springboothelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F01SpringBootDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(F01SpringBootDockerApplication.class, args);
+    }
+
+}
---
+++ F01-Docker/f01-spring-boot-docker/src/test/java/com/supremepole/a01springboothelloworld/F01SpringBootDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supremepole.a01springboothelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class F01SpringBootDockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
